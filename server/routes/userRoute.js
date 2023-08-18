3f4d99c50b377124ddf60d30c441c48f28de49f9
Biraj karki <birajkarki9849@gmail.com>
2023-08-18 12:50:49 +0545

    completd server side of application

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,13 +1,21 @@
 import express from "express";
+
 import {
-  createResidency,
-  getAllResidencies,
-  getResidency,
-} from "../controllers/resdCntrl.js";
-import jwtCheck from "../config/auth0Config.js";
+  bookVisit,
+  cancelBooking,
+  createUser,
+  getAllBookings,
+  getAllFavorites,
+  toFav,
+} from "../controllers/userCntrl.js";
+
 const router = express.Router();
 
-router.post("/create", jwtCheck, createResidency);
-router.get("/allresd", getAllResidencies);
-router.get("/:id", getResidency);
-export { router as residencyRoute };
+router.post("/register", createUser);
+router.post("/bookVisit/:id", bookVisit);
+router.post("/allBookings/", getAllBookings);
+router.post("/removeBooking/:id", cancelBooking);
+router.post("/toFav/:rid", toFav);
+router.post("/allFav/", getAllFavorites);
+
+export { router as userRoute };
