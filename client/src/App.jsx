cd4d16c7b5026f9897981cc00703980d08cbe926
Biraj Karki <birajkarki9849@gmail.com>
2023-08-19 18:43:25 +0545

    completed the client part

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import Website from "./pages/Website";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...........</div>}>
+        <Routes>
+          <Route path="/" element={<Website />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
+
+export default App;
